e4cf49ab60719e4e408272888eba5a6bcef8291c
Niklas Barning <niklas.barning@hmmh.de>
2019-05-16 09:18:04 +0200

    First sketch

--- 160518/sketch.js
+++ 160518/sketch.js
@@ -1,7 +1,27 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(500, 500);
+	drawLines();
 }
 
-function draw() {
+function mousePressed() {
+	drawLines();
+}
 
+function drawLines() {
+	background(255);
+	var lpX = width / 2;
+	var lpY = height / 2;
+
+	for (let i = 0; i < 50; i++) {
+		npX = random(lpX - 50, lpX + 50);
+		npY = random(lpY - 50, lpY + 50);
+
+		if (npX >= width) npX = npX * -1;
+		if (npY >= height) npY = npY * -1;
+
+		line(lpX, lpY, npX, npY);
+
+		lpX = npX;
+		lpY = npY;
+	}
 }
